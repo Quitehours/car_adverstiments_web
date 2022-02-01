b2d8c84bebd87389255dad9ead9d5dc05f3a4d4d
Dmytro Lukash <dmytro_lukash@epam.com>
2022-02-01 02:00:57 +0200

    Updated migration for User-device and delete comments

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_db_column(:email).of_type(:string).with_options(null: false, default: '') }
     it { is_expected.to have_db_column(:encrypted_password).of_type(:string).with_options(null: false, default: '') }
 
-    it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
-    it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
   end
 
   describe 'validations' do
